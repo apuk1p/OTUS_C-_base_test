bc9899418a986dfef5999e0f1f997610e2d133f4
Alexandr <apuk1p@mail.ru>
2023-06-02 17:56:52 +0300

    еще один вариант

--- Homework08/topk_words.cpp
+++ Homework08/topk_words.cpp
@@ -20,15 +20,15 @@ const size_t TOPK = 10;
 using Counter = std::map<std::string, std::size_t>;
 using WorkQueue = WaitingQueue<std::string>;
 
-std::condition_variable condition_variable;
-std::mutex mutex;
-bool notificated = false;
+// std::condition_variable condition_variable;
+// std::mutex mutex;
+// bool notificated = false;
 
 std::string tolower(const std::string &str);
 
-void count_words(std::istream& stream, Counter&);
+void count_words(WorkQueue&, std::istream& stream, Counter&);
 
-void print_topk(std::ostream& stream, const Counter&, const size_t k);
+void print_topk(WorkQueue& ,std::ostream& stream, const Counter&, const size_t k);
 
 int main(int argc, char *argv[]) {
 	if (argc < 2) {
@@ -62,11 +62,13 @@ int main(int argc, char *argv[]) {
 	// count_words(input, freq_dict);
 	std::thread reader{
 		count_words,
+		std::ref(work_queue),
 		std::ref(input),
 		std::ref(freq_dict)
 	};
 	std::thread writer{
 		print_topk,
+		std::ref(work_queue),
 		std::ref(std::cout),
 		std::cref(freq_dict),
 		TOPK
@@ -96,35 +98,45 @@ std::string tolower(const std::string &str) {
 	return lower_str;
 };
 
-void count_words(std::istream& stream, Counter& counter) {
-	std::unique_lock<std::mutex> lock(mutex);
+void count_words(WorkQueue& queue, std::istream& stream, Counter& counter) {
+	// std::unique_lock<std::mutex> lock(mutex);
 	std::for_each(std::istream_iterator<std::string>(stream),
 				std::istream_iterator<std::string>(),
-				 [&counter](const std::string &s) { ++counter[tolower(s)];
-				 condition_variable.notify_all();
+				 [&counter, &queue](const std::string &s) { ++counter[tolower(s)];
+				 queue.push(s);
 				 std::cout << "FIRST" << std::endl;
-				 notificated = true;
 				  });
+	queue.stop();
 }
 
-void print_topk(std::ostream& stream, const Counter& counter, const size_t k) {
-	std::unique_lock<std::mutex> lock(mutex);
-	while(!notificated) condition_variable.wait(lock);
-	std::cout << "SECOND" << std::endl;
+void print_topk(WorkQueue& queue, std::ostream& stream, const Counter& counter, const size_t k) {
+	std::string val;
+	// Counter words;
 	std::vector<Counter::const_iterator> words;
 	words.reserve(counter.size());
-	for (auto it = std::cbegin(counter); it != std::cend(counter); ++it) {
-		words.push_back(it);
-	}
+	auto it = std::begin(counter);
+	// auto itend = std::end(counter);
+	while(queue.pop(val))
+	{
+		std::cout << "SECOND " << std::endl;
+		// words.push_back(counter.find(val));
+	};
+	// std::vector<Counter::const_iterator> words;
+	// words.reserve(counter.size());
+	// for (auto it = std::cbegin(counter); it != std::cend(counter); ++it) {
+	// 	words.push_back(it);
+	// }
+	// auto it = words.begin();
+	// std::partial_sort(
+	// 	std::begin(words), std::begin(words) + k, std::end(words),
+	// 	[](auto lhs, auto &rhs) { return lhs->second > rhs->second; });
 
-	std::partial_sort(
-		std::begin(words), std::begin(words) + k, std::end(words),
-		[](auto lhs, auto &rhs) { return lhs->second > rhs->second; });
-
-	std::for_each(
-		std::begin(words), std::begin(words) + k,
-		[&stream](const Counter::const_iterator &pair) {
-			stream << std::setw(4) << pair->second << " " << pair->first
-					 << '\n';
-		});
+	// std::for_each(
+	// 	std::begin(words), std::begin(words) + k,
+	// 	[&stream](const Counter::const_iterator &pair) {
+	// 		stream << std::setw(4) << pair->second << " " << pair->first
+	// 				 << '\n';
+	// 	});
+	std::cout << " I'M HERE " << std::endl;
+	// queue.stop();
 }
