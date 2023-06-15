fc979198ecaeb1cd72df0dd7270e975801050340
Alexandr <apuk1p@mail.ru>
2023-06-15 23:37:53 +0300

    IDK

--- Homework08/topk_words.cpp
+++ Homework08/topk_words.cpp
@@ -1,5 +1,3 @@
-// Read files and prints top k word by frequency
-
 #include <algorithm>
 #include <cctype>
 #include <cstdlib>
@@ -10,25 +8,19 @@
 #include <map>
 #include <vector>
 #include <chrono>
-#include <future>
-#include <string>
-#include <numeric>
 #include "waiting_queue.h"
 
 const size_t TOPK = 10;
-
+int counter1 = 0;
 
 using Counter = std::map<std::string, std::size_t>;
-using WorkQueue = WaitingQueue<std::string>;
-using cntIterator = std::vector<Counter::const_iterator>;
+using WorkQueue = WaitingQueue<Counter>;
+
 
 std::string tolower(const std::string &str);
+void count_words(std::istream& stream, WorkQueue&, std::atomic<bool>& stop_flag);
+void print_topk(std::ostream& stream, WorkQueue&, const size_t k);
 
-void count_words(std::atomic<bool>&, WorkQueue&, std::istream& stream);
-
-void print_topk(WorkQueue& ,std::ostream& stream, Counter&, cntIterator&);
-
-void print(cntIterator& , const size_t k, std::ostream& stream);
 
 int main(int argc, char *argv[]) {
 	if (argc < 2) {
@@ -37,90 +29,91 @@ int main(int argc, char *argv[]) {
 	}
 
 	auto start = std::chrono::high_resolution_clock::now();
+	std::ifstream input;
+	std::atomic<bool> stop_flag = false;
 
 	Counter freq_dict;
 	WorkQueue work_queue;
-	cntIterator Words;
+	std::vector<std::string> filesArray;
+	std::vector<std::thread> threadPool;
 
-	std::ifstream input;
-	int n_threads;
-	std::atomic<bool> stop_flag = false;
+	for (int i = 1; i < argc; ++i) 
+	{
+		filesArray.push_back(argv[i]);
+	}
 
-	for (int i = 1; i < argc; ++i) {
-		if( i == 1) input.open(argv[i], std::ios_base::in);
+	threadPool.reserve(filesArray.size());
 
+	for(const auto& val : filesArray)
+	{
+		input.open(val, std::ios_base::in);
+		// std::ifstream input{val};
 		if (!input.is_open()) {
-			std::cerr << "Failed to open file " << argv[i] << '\n';
+			std::cerr << "Failed to open file " << val << '\n';
 			return EXIT_FAILURE;
 		}
-		n_threads = (i == 2) ? std::stoi( argv[2] ) : std::thread::hardware_concurrency();
-
-		if(n_threads < 1)
-		{
-			std::cerr << "threads nums should be positive\n";
-			return EXIT_FAILURE;
-		}
+		threadPool.push_back( std::thread( count_words, std::ref(input), std::ref(work_queue), std::ref(stop_flag)));
+		// count_words(input, work_queue);
+		input.close();
+		// freq_dict.merge(temp);
 	}
-	std::vector<std::thread> thrPool;
-	
-	for(int i = 1; i < n_threads; i++)
+
+	for(int i = 0; i < threadPool.size(); i++)
 	{
-		if(i == 1) thrPool.push_back(std::thread(count_words, std::ref(stop_flag), std::ref(work_queue), std::ref(input)));
-
-		thrPool.push_back(std::thread(print_topk, std::ref(work_queue), std::ref(std::cout), std::ref(freq_dict), std::ref(Words)));
+		threadPool[i].join();
 	}
-	for(int i = 0; i < thrPool.size(); i++)
+	while(work_queue.pop(freq_dict))
 	{
-		thrPool[i].join();
+		std::cout << "CHECING" << std::endl;
 	}
-
-	print(Words, TOPK, std::cout);
+	// stop_flag = true;
+	// work_queue.stop();
+	// print_topk(std::cout, work_queue, TOPK);
+	std::cout << "TEST1 " << counter1 << std::endl;
 	auto end = std::chrono::high_resolution_clock::now();
 	auto elapsed_ms = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
 	std::cout << "Elapsed time is " << elapsed_ms.count() << " us\n";
 }
 
 std::string tolower(const std::string &str) {
-	std::string lower_str;
-	auto lambda = [&](unsigned char ch) { return std::tolower(ch); };
-	auto rmLambda =[&lower_str](unsigned char c){ 
-					if( !std::isalpha(c) ){ lower_str.erase( std::remove( lower_str.begin(), lower_str.end(), c ), lower_str.end() ); }
-					};
-	std::transform(std::cbegin(str), std::cend(str),
-					std::back_inserter(lower_str),
-					lambda );
-	std::for_each(lower_str.begin(), lower_str.end(), 
-				rmLambda );
-	
+	std::string lower_str( str.size(), ' ' );
+	std::transform(str.begin(), str.end(), lower_str.begin(),
+				[](unsigned char ch) { return std::tolower(ch); });
+
+	//still incorrect
+	auto isNotAlpha = [](unsigned char c) { return !std::isalpha(c); };
+	lower_str.erase( std::remove_if( lower_str.begin(), lower_str.end(), isNotAlpha),  
+					lower_str.end() );
 	return lower_str;
 };
 
-void count_words(std::atomic<bool>& stop_flag, WorkQueue& queue, std::istream& stream) {
+void count_words(std::istream& stream, WorkQueue& queue, std::atomic<bool>& stop_flag) {
+	Counter counter;
 	std::for_each(std::istream_iterator<std::string>(stream),
 				std::istream_iterator<std::string>(),
-				 [&](const std::string &s) { 
-				 if(!stop_flag) queue.push(tolower(s));
-				  });
-				  
-	stop_flag = true;
-	queue.stop();
+				[&counter](const std::string &s) { ++counter[tolower(s)]; });
+	if(!stop_flag) queue.push(counter);
+	++counter1;
+	// queue.stop();
 }
 
-void print_topk(WorkQueue& queue, std::ostream& stream, Counter& counter, cntIterator& words) {
-	std::string val;
-
-	while(queue.pop(val))
+void print_topk(std::ostream& stream, WorkQueue& queue, const size_t k) {
+	std::vector<Counter::const_iterator> words;
+	Counter counter;
+	std::cout << queue.pop(counter) << std::endl;
+	while(queue.pop(counter))
 	{
-		++counter[val];
-	};
-
-	words.reserve(counter.size());
-
-	for (auto it = std::cbegin(counter); it != std::cend(counter); ++it) {
-		words.push_back(it);
+		std::cout << "TEST" << std::endl;
+		std::cout << queue.pop(counter) << std::endl;
+		// for(auto [k,v] : counter)
+		// {
+		// 	// std::cout << "4LEN" << std::endl;
+		// }
 	}
-	
 
+	// for (auto it = std::cbegin(counter); it != std::cend(counter); ++it) {
+	// 	words.push_back(it);
+	// }
 	// std::partial_sort(
 	// 	std::begin(words), std::begin(words) + k, std::end(words),
 	// 	[](auto lhs, auto &rhs) { return lhs->second > rhs->second; });
@@ -129,22 +122,6 @@ void print_topk(WorkQueue& queue, std::ostream& stream, Counter& counter, cntIte
 	// 	std::begin(words), std::begin(words) + k,
 	// 	[&stream](const Counter::const_iterator &pair) {
 	// 		stream << std::setw(4) << pair->second << " " << pair->first
-	// 				 << '\n';
+	// 				<< '\n';
 	// 	});
-	// std::cout << " I'M HERE " << std::endl;
-	// queue.stop();
-}
-
-void print(cntIterator& words, const size_t k, std::ostream& stream)
-{
-	std::partial_sort(
-		std::begin(words), std::begin(words) + k, std::end(words),
-		[](auto lhs, auto &rhs) { return lhs->second > rhs->second; });
-
-	std::for_each(
-		std::begin(words), std::begin(words) + k,
-		[&stream](const Counter::const_iterator &pair) {
-			stream << std::setw(4) << pair->second << " " << pair->first
-					 << '\n';
-		});
 }
